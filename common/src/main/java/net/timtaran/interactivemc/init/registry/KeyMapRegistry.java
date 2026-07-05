775bf4ce370b9a37424e0536c9e31d70f6af1e19
timtaran <tentaranka@gmail.com>
2026-07-05 19:22:54 +0500

    chore(docs): update KeyMapRegistry docstrings

--- common/src/main/java/net/timtaran/interactivemc/init/registry/KeyMapRegistry.java
+++ common/src/main/java/net/timtaran/interactivemc/init/registry/KeyMapRegistry.java
@@ -6,6 +6,7 @@ package net.timtaran.interactivemc.init.registry;
 
 import net.timtaran.interactivemc.mixin.bridge.vivecraft.KeyMappingHandlingMixin;
 import net.timtaran.interactivemc.mixin.bridge.vivecraft.KeyMappingRegisterMixin;
+import net.timtaran.interactivemc.mixin.bridge.vivecraft.VRInputActionMixin;
 import net.timtaran.interactivemc.util.InteractiveMCIdentifier;
 import org.vivecraft.client_vr.provider.HandedKeyBinding;
 
@@ -13,10 +14,13 @@ import org.vivecraft.client_vr.provider.HandedKeyBinding;
  * Class containing all mod keymappings.
  * Registration happens inside {@link KeyMappingRegisterMixin}.
  * Handling happens inside {@link KeyMappingHandlingMixin}.
+ * Others bindings prevention happens inside {@link VRInputActionMixin}
  *
  * @author timtaran
  */
 public class KeyMapRegistry {
+    // We are using `HandedKeyBinding` here, because vivecraft doesn't pass `ControllerType` to default KeyMapping's.
+
     /**
      * Main-Hand Trigger
      */
